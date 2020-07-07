b3ab83afc17fa2250da57df33205a3996a07413c
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-07-07 21:56:06 +0530

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "anagrams"
+
+scalaVersion := "2.12.0"
+
+scalacOptions ++= Seq("-deprecation")
+
+// Scala Test
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.0" % Test
---
+++ src/main/scala/anagrams/package.scala
@@ -0,0 +1,24 @@
+package object anagrams {
+  val dictionaryPath = List("anagrams", "linuxwords.txt")
+
+  def loadDictionary = {
+    val wordstream = Option {
+      getClass.getResourceAsStream(dictionaryPath.mkString("/"))
+    } orElse {
+      common.resourceAsStreamFromSrc(dictionaryPath)
+    } getOrElse {
+      sys.error("Could not load word list, dictionary file not found")
+    }
+    try {
+      val s = io.Source.fromInputStream(wordstream)
+      s.getLines.toList
+    } catch {
+      case e: Exception =>
+        println("Could not load word list: " + e)
+        throw e
+    } finally {
+      wordstream.close()
+    }
+  }
+
+}
